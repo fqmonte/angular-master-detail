b29eb915eda1695880addb5185698ed986909adf
Fabiano Monte <fabianowebes@gmail.com>
2020-11-14 22:39:50 -0300

    Adicionando o in-memory-database para categorias

---
+++ src/app/pages/categories/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { from } from 'rxjs';
+import { InMemoryDbService } from "angular-in-memory-web-api"
+
+export class inMemoryDatabase implements InMemoryDbService {
+    createDb() {
+        const categories = [
+            { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
+            { id: 2, name: 'Saúde', description: 'Plano de saúde' },
+            { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+            { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
+        ];
+
+        return { categories }
+    }
+}
