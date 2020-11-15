0febba4704e98a56a9f9df91f96c38b19fbb71ab
Fabiano Monte <fabianowebes@gmail.com>
2020-11-15 00:09:09 -0300

    Delete item do category-list

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+
+import { Category } from '../shared/category.model'
+import { CategoryService } from '../shared/category.service'
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = []
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm('Realmente deseja deletar?')
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Erro ao excluir")
+      )
+    }
   }
 
 }
